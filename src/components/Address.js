1c980a553ac9eca6f4c9cfacd6921aed4bf80627
Ewa Malusecka <maluseckae@gmail.com>
2019-02-14 14:40:24 +0000

    Automatically backed up by Learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -2,18 +2,11 @@
 import React form 'react';
 
 export default class Address extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fullAddress: `{this.props.steet}, {this.props.city}`
-    }
-  }
 
   render(){
     return (
       <div className="address">
-      {this.state.fullAddress}
+      {this.props.street}, {this.props.city}
       </div>
     )
   }
